0d60f39586652599568bd4b7bccc99a88cc82d89
HitalloMarinho <126522834+HitalloMarinho@users.noreply.github.com>
2023-03-14 20:28:24 -0300

    js

--- exec 2/operadores.js
+++ exec 2/operadores.js
@@ -1,6 +1,7 @@
 var valor1 = 1
 var valor2 = 4
-var soma = valor1 + valor2
+var valor3 = 5
+var soma = valor1 + valor2 * valor3  //- + / *
 console.log(`A soma dos valores é ${soma}`) //shift com `.
 
 //(número + número) - Soma
