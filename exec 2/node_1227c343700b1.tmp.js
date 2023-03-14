d61587385b928564a0ace10b54d375eb84a3e29c
HitalloMarinho <126522834+HitalloMarinho@users.noreply.github.com>
2023-03-14 19:54:24 -0300

    js

---
+++ exec 2/node_1227c343700b1.tmp.js
@@ -0,0 +1,7 @@
+var valor1 = 1
+var valor2 = 4
+var soma = valor1 + valor2
+console.log(`A soma dos valores é ${soma}`) //shift com `.
+
+//(número + número) - Soma
+//(string + string) - Concatenação
