aaa5eab973f7dfb0ed3fff911102bee2433a249c
HitalloMarinho <126522834+HitalloMarinho@users.noreply.github.com>
2023-03-20 19:06:09 -0300

    exec 8

---
+++ exec 8/contador.js
@@ -0,0 +1,28 @@
+var x = 2 // declarando uma variavel chamada x e ela recebe 2.
+var y = 5 // declarando uma variavel chamada y e ela recebe 5.
+
+console.log(`O valor de x é ${x}`) // Exibe a mensagem (valor de x) no console.
+
+console.log(`O valor de y é ${y}`) // Exibe a mensagem (valor de y) no console. 
+
+var soma = x + y
+
+console.log(`A soma dos valores ${x} e ${y} é ${soma}`)
+
+soma += x // Isso e o mesmo que soma = soma + x
+
+console.log(`O valor de soma agora é ${soma}`)
+
+var contador = 5
+
+console.log(`O valor do contador é ${contador}`)
+contador += 1 //contador = contador + 1
+console.log(`O novo valor do contador é ${contador}`)
+
+var cont = 0
+
+console.log(`O valor de cont é: ${cont}`) 
+cont ++
+console.log(`O valor de cont é: ${cont}`)
+cont = 0
+console.log(`O valor de cont é: ${cont}`)
