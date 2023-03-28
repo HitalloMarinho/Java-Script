8b22ae2801e5d444f6b1e3e446beded78efac3fc
HitalloMarinho <126522834+HitalloMarinho@users.noreply.github.com>
2023-03-28 20:42:42 -0300

    exec 26

--- exec 26/script.js
+++ exec 26/script.js
@@ -1,9 +1,16 @@
 function descobrir(){
     var numero = Number.parseInt(window.prompt(`Digite um Número:`))
 
-    if (numero % 2 == 0) {
-        document.getElementById("resultado").innerHTML = `<p>O número ${numero} é par.</p>`
-    } else {
-        document.getElementById("resultado").innerHTML = `<p>O número ${numero} é ímpar.</p>`
-    }
+    var resto = numero % 2
+
+
+
+    switch (resto) {
+        case 0:
+            document.getElementById("resultado").innerHTML = `<p>O número ${numero} é par.</p>`
+            break
+        case 1:
+            document.getElementById("resultado").innerHTML = `<p>O número ${numero} é ímpar.</p>`
+            break
+    }    
 }
