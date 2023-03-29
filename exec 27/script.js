94c1f4945a0453914a3b3e7d1c9bc602f7245975
HitalloMarinho <126522834+HitalloMarinho@users.noreply.github.com>
2023-03-29 18:54:30 -0300

    0

---
+++ exec 27/script.js
@@ -0,0 +1,12 @@
+for (var cont = 0; cont <=1; cont ++){
+    console.log(`O contador está no passo ${cont}.`)
+}
+
+/* for (var cont = 5; cont >=0; cont --){     --CONTAGEM REGRESSIVA--
+    console.log(`O contador está no passo ${cont}.`)
+} */
+
+/* for (var cont = 0; cont <=10; cont +=2){     --CONTAGEM DE 2 EM 2
+    console.log(`O contador está no passo ${cont}.`)
+} */
+
