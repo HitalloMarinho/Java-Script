a3b4568dbe7a701eeca3a20d360f6e741b5cc4d2
HitalloMarinho <126522834+HitalloMarinho@users.noreply.github.com>
2023-03-30 20:58:57 -0300

    1

--- teste LOOP/script.js
+++ teste LOOP/script.js
@@ -1,16 +1,9 @@
-/* for (var i = 1; i < 6; i++) {
-  console.log(`#### Loop ${i} ####`);
-  do (var j = 1; j <= 5; j++) {
-    console.log(j);
-  } while
-}
- */
+var vezes = 1
 
-var cont = i = 1; i < 6; i++
-
-do {
-  console.log(`Passo ${cont}`)
-  cont++
-} while ('j = 1; j <= 5; j++') {
-  console.log(j);
+while (vezes <= 5){
+  console.log(`#### Loop ${vezes} ####`)
+  for(var i = 1; i <= 5; i++) {
+  console.log(`Passo ${i}.`)
+  }
+  vezes++
 }
