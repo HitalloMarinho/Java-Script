db220325845feb998c2f82818fec5d9ff78572b8
HitalloMarinho <126522834+HitalloMarinho@users.noreply.github.com>
2023-03-30 20:15:36 -0300

    0

--- exec 30/script.js
+++ exec 30/script.js
@@ -1,6 +1,6 @@
-for (var i = 1; i < 6; i++) {   
-    console.log(`#### Loop ${i} ####`); 
-    for (var j = 1; j <= 5; j++) { 
-      console.log(j);     
+for (var i = 1; i < 6; i++) {    // loop para cada parte
+    console.log(`#### Loop ${i} ####`);  // imprime o número da parte
+    for (var j = 1; j <= 5; j++) {   // loop para cada número dentro da parte
+      console.log(j);     // imprime o número atual
     }
   }
--- teste LOOP/script.js
+++ teste LOOP/script.js
@@ -1,7 +1,7 @@
-for (var i = 1; i < 6; i++) {   // loop para cada parte
-    console.log(`#### Loop ${i} ####`);  // imprime o número da parte
-    for (var j = 1; j <= 5; j++) { // loop para cada número dentro da parte
-      console.log(j);      // imprime o número atual
-    }
+for (var i = 1; i < 6; i++) {   
+    console.log(`#### Loop ${i} ####`); 
+    do (var j = 1; j <= 5; j++) { 
+      console.log(j);      
+    } while
   }
   
