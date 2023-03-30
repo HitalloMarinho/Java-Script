5db9fc446c9666c59b85ecbec8fd40d678dcda79
HitalloMarinho <126522834+HitalloMarinho@users.noreply.github.com>
2023-03-30 18:40:46 -0300

    0

---
+++ exec 30/script.js
@@ -0,0 +1,8 @@
+for (var x = 1; x <= 2; x++) {
+    for (var y = 3; y <= 4; y++) {
+        console.log(`Passo ${x}.${y}`)
+        for (var y = 1; y <= 2; y++) {
+            console.log(`Passo ${x}.${y}`)
+        }
+    }
+}
