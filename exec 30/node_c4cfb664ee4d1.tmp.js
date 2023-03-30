091092555bdc7e579928f96d2a0b45f32f946bee
HitalloMarinho <126522834+HitalloMarinho@users.noreply.github.com>
2023-03-30 19:04:54 -0300

    0

--- exec 30/node_c4cfb664ee4d1.tmp.js
+++
@@ -1,9 +0,0 @@
-for (var x = 2; x <= 2; x++) { 
-    console.log(`#### Loop 1 #### `)
-    for (var y = 1; y <= 2; y++) {
-        console.log(` ${x}.${y}`)
-    }
-    for (var y = 3; ; y++) {
-        console.log(` ${x}.${y}`)
-    }
-}
--- exec 30/script.js
+++ exec 30/script.js
@@ -1,9 +1,42 @@
-for (var x = 2; x <= 2; x++) { 
+for (var x = 1; x <= 1; x++) { 
     console.log(`#### Loop 1 #### `)
-    for (var y = 1; y <= 2; y++) {
-        console.log(` ${x}.${y}`)
+    for (var y = 1; y <= 1; y++) {
+        console.log(` ${x}.`)
     }
-    for (var y = 3; y <= 5; y++) {
-        console.log(` ${x}.${y}`)
+    for (var y = 2; y <= 5; y++) {
+        console.log(` ${y}.`)
     }
+
+    console.log(`#### Loop 2 #### `)
+    for (var y = 1; y <= 1; y++) {
+        console.log(` ${x}.`)
+    }
+    for (var y = 2; y <= 5; y++) {
+        console.log(` ${y}.`)
+    }
+    
+    console.log(`#### Loop 3 #### `)
+    for (var y = 1; y <= 1; y++) {
+        console.log(` ${x}.`)
+    }
+    for (var y = 2; y <= 5; y++) {
+        console.log(` ${y}.`)
+    }
+
+    console.log(`#### Loop 4 #### `)
+    for (var y = 1; y <= 1; y++) {
+        console.log(` ${x}.`)
+    }
+    for (var y = 2; y <= 5; y++) {
+        console.log(` ${y}.`)
+    }
+
+    console.log(`#### Loop 5 #### `)
+    for (var y = 1; y <= 1; y++) {
+        console.log(` ${x}.`)
+    }
+    for (var y = 2; y <= 5; y++) {
+        console.log(` ${y}.`)
+    }
+
 }
