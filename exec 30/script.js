9595920399ff5c8c19b0043942473b46fdec766e
HitalloMarinho <126522834+HitalloMarinho@users.noreply.github.com>
2023-03-30 20:25:50 -0300

    0

--- exec 30/script.js
+++ exec 30/script.js
@@ -1,4 +1,4 @@
-for (var i = 1; i < 6; i++) {    // loop para cada parte
+for (var i = 1; i <= 5; i++) {    // loop para cada parte
     console.log(`#### Loop ${i} ####`);  // imprime o número da parte
     for (var j = 1; j <= 5; j++) {   // loop para cada número dentro da parte
       console.log(j);     // imprime o número atual
