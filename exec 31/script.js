93650c5a6c8c2cb9f0445f06de73e6fd1e2b2256
HitalloMarinho <126522834+HitalloMarinho@users.noreply.github.com>
2023-03-31 21:28:43 -0300

    1

---
+++ exec 31/script.js
@@ -0,0 +1,9 @@
+function contar() {
+    var vezes = Number.parseInt(document.getElementById(inicio).value)
+    var vezes2 = Number.parseInt(document.getElementById(fim).value)
+
+    while (vezes < 10) {
+        resultado.innerHTML = `Passo ${vezes}.`
+        }
+        vezes2++
+    }
