7d7b0baeee677e0d68bbe7a836f8919060f3ce00
HitalloMarinho <126522834+HitalloMarinho@users.noreply.github.com>
2023-04-13 18:58:48 -0300

    exec 31

--- exec 31/script.js
+++ exec 31/script.js
@@ -1,9 +1,32 @@
 function contar() {
-    var vezes = Number.parseInt(document.getElementById(inicio).value)
-    var vezes2 = Number.parseInt(document.getElementById(fim).value)
+    var inicio = Number(document.getElementById('inicio').value)
+    var fim = Number(document.getElementById('fim').value)
+    var passo = Number(document.getElementById('passo').value)
 
-    while (vezes < 10) {
-        resultado.innerHTML = `Passo ${vezes}.`
+    console.log(`Valor do Inicio: ${inicio}`)
+    console.log(`Valor do Fim: ${fim}`)
+    console.log(`Valor do Passo: ${passo}`)
+    var resultado = document.getElementById('resultado')
+    resultado.innerHTML = ""
+    if (passo <= 0) {
+        resultado.innerHTML = 'O passo não pode ser 0 ou menor.'
+    } else if (inicio < fim) {
+        while (inicio <= fim) {
+            if (inicio + passo > fim) {
+                resultado.innerHTML += `${inicio}\u{1F3C1}`
+            } else {
+                resultado.innerHTML += `${inicio}\u{27A1}`
+            }
+            inicio += passo
         }
-        vezes2++
+    } else {
+        while (inicio >= fim) {
+            if (inicio + passo > fim) {
+                resultado.innerHTML += `${inicio}\u{1F3C1}`
+            } else {
+                resultado.innerHTML += `${inicio}\u{27A1}`
+            }
+            inicio += passo
+        }
     }
+}
