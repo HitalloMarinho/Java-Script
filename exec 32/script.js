cc0ade64b4419f17248fca3e686a57460b6d6d9e
HitalloMarinho <126522834+HitalloMarinho@users.noreply.github.com>
2023-04-13 21:32:00 -0300

    0

--- exec 32/script.js
+++ exec 32/script.js
@@ -1,12 +1,17 @@
 function multiplicar() {
     var numero1 = Number(document.getElementById('numero').value)
     var numero2 = 1
-    var soma = numero1 * numero2
 
-    while (numero2 < 10) {
-        resultado.innerHTML = `<p> ${numero1} x ${numero2} = ${soma} </p>`
-        for (var i = 1; i <= 10; i++) {
+    resultado.innerHTML = `<h1> Tabuada de ${numero1}`
+
+    while (numero2 < 11) {
+
+        var soma = numero1 * numero2
+
+        for (var i = 1; i <= 1; i++) {
+            resultado.innerHTML += `<p> ${numero1} x ${numero2} = ${soma} </p>`
         }
         numero2++
     }
+    
 }
