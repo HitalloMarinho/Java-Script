3a23c916316cb93e6b328a1232a673f78b6d9ef8
HitalloMarinho <126522834+HitalloMarinho@users.noreply.github.com>
2023-04-26 21:41:12 -0300

    0

--- exec 37/script.js
+++ exec 37/script.js
@@ -4,12 +4,13 @@ var lista = document.getElementById('lista')
 
 function cadastrar() {
     if (nome.value != '') {
-        nomes.push(nome.value)
-        nomes.value = '' // Vai limpar o campo para digitar um novo nome.
-        nomes.focus()    // Vai deixar o cursor piscando dentro do campo nome.
-        
-        if (sf) {
-
+        if (nomes.indexOf(nome.value)== -1) {
+            nomes.push(nome.value)
+            nome.value = ''  // Vai limpar o campo para digitar um novo nome.
+            nome.focus()     // Vai deixar o cursor piscando dentro do campo nome.
+        } else {
+            alert(`Burro(a)! o nome ${nome.value} já foi desgraça ruim...`)
+            nome.focus()
         }
 
     } else {
@@ -19,7 +20,7 @@ function cadastrar() {
 }
 
 function imprimir() {
-    nome.innerHTML = ''
+    lista.innerHTML = ''
     for (var c = 0; c < nomes.length; c++) {
         lista.innerHTML += `<p>Aluno ${nomes[c]}</p>`
     }
---
+++ exec 38/script.js
@@ -0,0 +1,16 @@
+var Limpar = document.getElementById('Limpar')
+var numero = document.getElementById('number1')
+var Analisar = document.getElementById('Analisar')
+var valores = []
+
+function Adicionar() {
+
+}
+
+function Analisar() {
+
+}
+
+function Limpar() {
+
+}
