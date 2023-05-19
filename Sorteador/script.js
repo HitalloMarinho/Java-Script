2c866f50e6b829d38b3408597ff3b1853252fce9
HitalloMarinho <126522834+HitalloMarinho@users.noreply.github.com>
2023-05-18 21:37:54 -0300

    1

--- Sorteador/script.js
+++ Sorteador/script.js
@@ -3,11 +3,22 @@ function gerar() {
     var max = Number(document.getElementById('segundo_numero').value)
     var resultado = document.getElementById('resultado')
 
+    var numeros = [];
+    for (var i = min; i <= max; i++) {
+      numeros.push(i);
+    }
+
+    // Exibe o número sorteado
+    //resultado.innerHTML += "Número sorteado: " + numeroSorteado;
+
+
 /*  console.log(`O valor de max é ${max} e o seu tipo é ${typeof (max)}`)
     console.log(`O valor de max é ${min} e o seu tipo é ${typeof (min)}`) */
 
-    var randomInt = Math.floor(Math.random() * (max - min + 1) + min);
-    resultado.innerHTML += `<p>${randomInt}</p>`;
+    var index = Math.floor(Math.random() * (max - min + 1) + min);
+    var numeroSorteado = numeros.splice(index, 1)[0];
+    resultado.innerHTML += `${numeroSorteado}-`;
+
 }
 
 function limpar() {
